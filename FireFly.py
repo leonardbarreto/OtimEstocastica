dd0c77e45216742dcf451b8e805a2f7f8e3dd9bf
Leonard <leonardbarreto@gmail.com>
2012-09-06 10:11:33 -0300

    Inclusão dos métodos: get_mais_brilhante e get_menos_brilhante

--- FireFly.py
+++ FireFly.py
@@ -25,9 +25,13 @@ class FireFly:
         """ Criar e calcular a intensidade de emissão de luz de cada firefly. A intensidade de luz (Ii) de cada firefly (FFi) é determinado pelo valor f(xi). 
             Parâmetros:
                 - _pFflies -> populacao de fireflies"""
-        self._intensidade_luz=[]
+        _int_luz=[]
         for i in range(self._qtd_fireflies):
-            self._intensidade_luz.append(self.funcao(_pFflies[i]))
+            _int_luz.append(self.funcao(_pFflies[i]))
+        self._intensidade_de_luz=_int_luz
+    
+    def get_intensidade_emitida_de_luz(self):
+        return self._intensidade_de_luz
     
     def calcular_intensidade_percebida_de_luz(self,_pR,_pGama):
         """ Calcular a intensidade percebida de luz de cada firefly em relação aos demais. 
@@ -43,8 +47,8 @@ class FireFly:
                 - _p_dimen -> dimensão do problema
                 - _p_qtdff -> Quantidade de fireflies do problema
         """        
-        self._fFlies=[]  #população de fireflies
-        for i in range(_p_qtdff): #criar quantidade de fireflies definida...
+        self._fFlies=[]                         #população de fireflies
+        for i in range(_p_qtdff):               #criar quantidade de fireflies definida...
             self._fFlies.append(rand(_p_dimen)) #...atribuindo valores reais para cada um entre 0..1
         self.set_intensidade_de_luz(self._fFlies)
         
@@ -54,6 +58,40 @@ class FireFly:
     def get_intensidade_de_luz(self):
         return self._intensidade_luz
     
+    def get_mais_brilhante(self,_p_iel):
+        """ Retorna o firefly mais brilhante, ou seja, com maior valor na função objetivo.
+            Parâmetros:
+                - _p_iel -> Intensidade Emitida de Luz de cada firefly (vetor)
+            Retorno:
+                - _pMab -> Identifica o firefly mais brilhante (posição)
+                - _vMab -> Intensidade de brilho (valor da função objetivo) do respectivo firefly
+        """
+        _pMab=0                         # posição do mais brilhante
+        _vMab=self.funcao(_p_iel[0])    # valor do mais brilhante
+        for i in range(1,self._qtd_fireflies):
+            _v=self.funcao(_p_iel[i])            
+            if (_v>_pMab):
+                _pMab=i
+                _vMab=_v
+        return _pMab,_vMab
+                        
+    def get_menos_brilhante(self,_p_iel):
+        """ Retorna o firefly menos brilhante, ou seja, com maior valor na função objetivo.
+            Parâmetros:
+                - _p_iel -> Intensidade Emitida de Luz de cada firefly (vetor)
+            Retorno:
+                - _pMab -> Identifica o firefly mais brilhante (posição)
+                - _vMab -> Intensidade de brilho (valor da função objetivo) do respectivo firefly
+        """
+        _pmeb=0                         # posição do menos brilhante
+        _vmeb=self.funcao(_p_iel[0])    # valor do mais brilhante
+        for i in range(1,self._qtd_fireflies):
+            _v=self.funcao(_p_iel[i])            
+            if (_v<_pmeb):
+                _pmeb=i
+                _vmeb=_v
+        return _pmeb,_vmeb
+        
     def set_distancia_euclidiana(self,_fforig,_ffdest):
         """ Retorna a distância euclidiana entre o firefly origem e destino.
             Parâmetros:
@@ -62,11 +100,11 @@ class FireFly:
             Retorno:
                 - _r -> distância euclidiana entre os dois pontos
         """
-       _r=0
-       for j in range(self._dimen): #calcula em função da dimensão do problema
-           _r=_r+((_fforig[j]-_ffdest[j])**2)
-       _r=sqrt(_r)
-       return _r
+        _r=0
+        for j in range(self._dimen): #calcula em função da dimensão do problema
+            _r=_r+((_fforig[j]-_ffdest[j])**2)
+        _r=sqrt(_r)
+        return _r
        # rij - Distãncia r entre o firefly(i) e o firefly(j)
         
         
